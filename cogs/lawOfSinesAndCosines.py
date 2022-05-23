ebb7a370a6a1ca9bfac91bdb3cffe8b3e0220a6e
Necrocaustic <47093311+Necrocaustic@users.noreply.github.com>
2022-05-22 22:26:02 -0400

    Changed callCosSide to callCosAngle to more accurately reflect the purpose of the funciton. Added callable functions for the following: Sine to solve for angle Sine to solve for side.

--- cogs/lawOfSinesAndCosines.py
+++ cogs/lawOfSinesAndCosines.py
@@ -2,29 +2,41 @@ import math
 
 
 def cosines():
-    goal = input('Are you looking for a side or a angle?')
-    if goal == 'angle':
-        sidea = float(input('What is the length of side A?'))
-        sideb = float(input('What is the length of side B?'))
-        sidec = float(input('What is the length of side C?'))
-        angleARads = math.acos(((sideb ** 2) + (sidec ** 2) - (sidea ** 2)) / (2 * sideb * sidec))
-        angleBRads = math.acos(((sidea ** 2) + (sidec ** 2) - (sideb ** 2)) / (2 * sidea * sidec))
-        angleCRads = math.acos(((sideb ** 2) + (sidea ** 2) - (sidec ** 2)) / (2 * sideb * sidea))
-        angleA = math.degrees(angleARads)
-        angleB = math.degrees(angleBRads)
-        angleC = math.degrees(angleCRads)
+    SSA = input('Is the case SSA? (y/n')
+    if SSA != 'y':
+        goal = input('Are you looking for a side or a angle?')
+        if goal == 'angle':
+            sidea = float(input('What is the length of side A?'))
+            sideb = float(input('What is the length of side B?'))
+            sidec = float(input('What is the length of side C?'))
+            angleARads = math.acos(((sideb ** 2) + (sidec ** 2) - (sidea ** 2)) / (2 * sideb * sidec))
+            angleBRads = math.acos(((sidea ** 2) + (sidec ** 2) - (sideb ** 2)) / (2 * sidea * sidec))
+            angleCRads = math.acos(((sideb ** 2) + (sidea ** 2) - (sidec ** 2)) / (2 * sideb * sidea))
+            angleA = math.degrees(angleARads)
+            angleB = math.degrees(angleBRads)
+            angleC = math.degrees(angleCRads)
+            print(f'Side A: {sidea} | Side B: {sideb} | Side C: {sidec}')
+            print(f'Angle A: {angleA} | Angle B: {angleB} | Angle C: {angleC}')
+        else:
+            sidea = float(input('What is the length of side A?'))
+            sideb = float(input('What is the length of side B?'))
+            angleC = float(input('What is the value of Angle C (in deg.)?'))
+            angleCRads = math.radians(angleC)
+            sidec = math.sqrt((sidea ** 2) + (sideb ** 2) - (2 * sidea * sideb * math.cos(angleCRads)))
+            angles = callCosSide(sidea, sideb, sidec)
+            print(f'Side A: {sidea} | Side B: {sideb} | Side C: {sidec}')
+            print(f'Angle A: {angles[0]} | Angle B: {angles[1]} | Angle C: {angles[2]}')
+    else:
+        sidea = float(input('What is the measure of side A'))
+        angleA = float(input('What is the measure of angle A (in deg.)?'))
+        sidec = float(input('What is the measure of side C?'))
+        angleC = callSineAngle(sidea, angleA, sidec)
+        angleB = 180 - angleA - angleC
+        sideb = callSineSide(sidea, angleA, angleB)
         print(f'Side A: {sidea} | Side B: {sideb} | Side C: {sidec}')
         print(f'Angle A: {angleA} | Angle B: {angleB} | Angle C: {angleC}')
 
-    else:
-        sidea = float(input('What is the length of side A?'))
-        sideb = float(input('What is the length of side B?'))
-        angleC = float(input('What is the value of Angle C (in deg.)?'))
-        angleCRads = math.radians(angleC)
-        sidec = math.sqrt((sidea ** 2) + (sideb ** 2) - (2 * sidea * sideb * math.cos(angleCRads)))
-        angles = callCosSide(sidea, sideb, sidec)
-        print(f'Side A: {sidea} | Side B: {sideb} | Side C: {sidec}')
-        print(f'Angle A: {angles[0]} | Angle B: {angles[1]} | Angle C: {angles[2]}')
+
 
 
 def sines():
@@ -47,7 +59,7 @@ def sines():
         print(f'The length of the side is: {sideb}.')
 
 
-def callCosSide(sidea, sideb, sidec):
+def callCosAngle(sidea, sideb, sidec):
     angleARads = math.acos(((sideb ** 2) + (sidec ** 2) - (sidea ** 2)) / (2 * sideb * sidec))
     angleBRads = math.acos(((sidea ** 2) + (sidec ** 2) - (sideb ** 2)) / (2 * sidea * sidec))
     angleCRads = math.acos(((sideb ** 2) + (sidea ** 2) - (sidec ** 2)) / (2 * sideb * sidea))
@@ -56,3 +68,47 @@ def callCosSide(sidea, sideb, sidec):
     angleC = math.degrees(angleCRads)
     angles = [angleA, angleB, angleC]
     return angles
+
+
+
+
+
+def callSineSide(sidea, angleA, angleB):
+    angleBRads = math.radians(angleB)
+    angleARads = math.radians(angleA)
+    sideb = (sidea * math.sin(angleBRads)) / math.sin(angleARads)
+    return sideb
+
+def callSineAngle(sidea, angleA, sideb):
+    angleARads = math.radians(angleA)
+    angleBRads = math.asin((sideb * math.sin(angleARads)) / sidea)
+    angleB = math.degrees(angleBRads)
+    return angleB
+
+
+def ambiguity():
+    # the cases of purely comparing side lengths are commented out because h is fine by itself it to
+    # determine how many tringles there are.
+    angleA = float(input('What is the measure of Angle A (in deg.)?'))
+    sidea = float(input('What is the measure of Side A?'))
+    sideb = float(input('What is the measure of Side B?'))
+    angleARads = math.radians(angleA)
+    h = sideb * math.sin(angleARads)
+    if angleA > 90:
+        if sidea > sideb:
+            print('There is one possible triangle')
+        elif sidea <= sideb:
+            print('There is no possible triangles.')
+    elif angleA < 90:
+        if sidea < h:
+            print('There is no possible triangle')
+        #       elif sidea < sideb:
+        #           print('There is no possible triangle')
+        elif sidea == h:
+            print('There is one possible triangle')
+        #      elif sidea >= sideb:
+        #          print('There is one possible triangle')
+        elif h < sidea:
+            print('There are two possible triangles')
+#       elif sidea < sideb:
+#         print('There are two possible triangles')
--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from cogs.oblTriArea import *
 from cogs.trigFuncs import *
 from cogs.lawOfSinesAndCosines import *
 
+
 def main():
     typeOfTrig = input('What type of trig is required?\n'
                        'Options are:\n '
                        '| A - Trig functions |\n '
                        '| B - Obl Tris |\n'
                        '| C - Law of Sines |\n '
-                       '| D - Law of Cosines |\n '
-                       'Answer using A, B, C, or D.')
+                       '| D - Law of Cosines |\n'
+                       '| E - Check for Ambiguity | \n '
+                       'Answer using A, B, C, D, or E.')
     if typeOfTrig == 'A':
         trigFuncs()
     elif typeOfTrig == 'B':
@@ -19,6 +21,8 @@ def main():
         sines()
     elif typeOfTrig == 'D':
         cosines()
+    elif typeOfTrig == 'E':
+        ambiguity()
     else:
         print('Not a valid response. Terminating program.')
 
